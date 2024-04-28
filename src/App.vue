5daa717ae0cb5e07e8823afced7514d8843f4774
bvstivn <bbmmllvv@gmail.com>
2024-04-28 11:55:50 -0400

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 class="titulo">Buscador de Clima</h1>
+  <div class="contenedor buscador-clima"></div>
+  <Formulario @obtener-clima="obtenerClima" />
+  <Spinner v-if="cargando" />
+  <Alerta v-if="error">{{ error }}</Alerta>
+  <Clima v-if="mostrarClima" :clima="clima" />
+</template>
+
+<script setup>
+//Components
+import Formulario from './components/Formulario.vue';
+import Clima from './components/Clima.vue';
+import Spinner from './components/Spinner.vue';
+import Alerta from './components/Alerta.vue';
+
+//Composables
+import useClima from '../composables/useClima';
+
+//Composables
+const { obtenerClima, clima, mostrarClima, cargando, error } = useClima();
+
+</script>
---
+++ src/components/Clima.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="resultado" >
+        <h2>Clima de: {{ clima.name }} </h2>
+        <p class="actual" >{{ formatearTemperatura(clima.main.tem) }} &deg;C</p>
+        <div class="temperaturas">
+            <p>Min: <span> {{ formatearTemperatura(clima.main.temp_min) }} &deg;C</span></p>
+            <p>Max: <span> {{ formatearTemperatura(clima.main.temp_max) }} &deg;C</span></p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+//Composables
+import useClima from "../composables/useClima";
+
+//Composables
+const { formatearTemperatura } = useClima();
+
+//Definiciones
+defineProps({
+    clima:{
+        type: Object,
+        required: true,
+    }
+});
+</script>
